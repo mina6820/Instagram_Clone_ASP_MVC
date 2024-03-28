329821274570b94e0c3b72af38518509ff555d65
Mina Medhat <minamedhat123456@gmail.com>
2024-03-28 21:27:16 +0200

    delete

--- Instagram_Clone/Models/photo/postPhoto.cs
+++ Instagram_Clone/Models/photo/postPhoto.cs
@@ -7,13 +7,6 @@ namespace Instagram_Clone.Models.photo
         [ForeignKey("Post")]
         public int PostId { get; set; }
         public Post Post { get; set; }
-        public string ayhaga { get; set; }
-        public string ayhaga2 { get; set; }
-        public string ayhaga22 { get; set; }
-        //////comment 33333
-        // comment 4444444
-        //comment 5555
-        //comment 666666
-        //comment 7
+
     }
 }
