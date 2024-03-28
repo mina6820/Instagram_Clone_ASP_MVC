411226f4a04688f93059390c89518c960db1265a
mohammedali882001 <leomohammed882001@gmail.com>
2024-03-28 09:17:32 -0700

    comment3

--- Instagram_Clone/Models/photo/postPhoto.cs
+++ Instagram_Clone/Models/photo/postPhoto.cs
@@ -10,6 +10,7 @@ namespace Instagram_Clone.Models.photo
         public string ayhaga { get; set; }
         public string ayhaga2 { get; set; }
         public string ayhaga22 { get; set; }
+        //////comment 33333
 
     }
 }
