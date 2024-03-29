2a555f188f50ba8a514f1ced2cfb1238bfdcbad3
Abader <abadeerdodoesmut@gmail.com>
2024-03-30 00:18:39 +0200

    getFollowers/Following / Search

--- Instagram_Clone/Repositories/UserRelationship/IUserRelationshipRepository.cs
+++ Instagram_Clone/Repositories/UserRelationship/IUserRelationshipRepository.cs
@@ -4,6 +4,18 @@ namespace Instagram_Clone.Repositories.UserFollowRepo
 {
     public interface IUserRelationshipRepository : IRepository<UserRelationship>
     {
-        //public List<ApplicationUser> GetFollowers(string id);
+        public List<ApplicationUser> GetFollowers(string id);
+
+        public ApplicationUser GetById(string id);
+
+        public List<ApplicationUser> GetFollowees(string id);
+
+        public List<ApplicationUser> searchFollowers(string id, string name);
+
+        public List<ApplicationUser> searchFollowees(string id, string name);
+
+
+
+
     }
 }
