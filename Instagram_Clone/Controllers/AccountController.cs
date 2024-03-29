958db8b98c73bc877b2185d48b6aa67c130ff38e
Mohammed Ali <80027329@fci.s-mu.edu.eg>
2024-03-29 13:42:31 -0800

    make login and logout actions

--- Instagram_Clone/Controllers/AccountController.cs
+++ Instagram_Clone/Controllers/AccountController.cs
@@ -1,5 +1,7 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿using Instagram_Clone.ViewModels;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
 
 namespace Instagram_Clone.Controllers
 {
@@ -22,6 +24,35 @@ namespace Instagram_Clone.Controllers
         public IActionResult Login()
         {
             return View("Login");
+        }
+
+        public async Task<IActionResult> SaveLogin(LoginViewModel loginViewModel)
+        {
+            if (ModelState.IsValid == true)
+            {
+                ApplicationUser applicationUserDB = await userManager.FindByNameAsync(loginViewModel.Username);
+                if (applicationUserDB != null)
+                {
+                    bool found = await userManager.CheckPasswordAsync(applicationUserDB, loginViewModel.Password);
+                    if (found == true)
+                    {
+                        //List<Claim> claims = new List<Claim>();
+                        //claims.Add(new Claim("Institute", "ITI"));
+                        ////await  signInManager.SignInWithClaimsAsync(applicationUserDB,loginViewModel.RememberMe, claims);   
+                        await signInManager.SignInAsync(applicationUserDB, loginViewModel.RememberMe);
+                        return RedirectToAction("Index", "MarwaAndMessiTest");
+                    }
+                }
+
+                ModelState.AddModelError("", "Invalid Data Please Try Again");
+            }
+            return View("Login", loginViewModel);
+        }
+
+        public async Task<IActionResult> LogOut()
+        {
+            await signInManager.SignOutAsync();
+            return RedirectToAction("Login");
         }
     }
 }
