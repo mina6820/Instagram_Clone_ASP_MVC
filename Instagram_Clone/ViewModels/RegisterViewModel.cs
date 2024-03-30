6cf8649e4255948d9dde5c46bf67de848f7bf286
Mohammed Ali <80027329@fci.s-mu.edu.eg>
2024-03-29 16:14:35 -0800

    Add Unique UserName

--- Instagram_Clone/ViewModels/RegisterViewModel.cs
+++ Instagram_Clone/ViewModels/RegisterViewModel.cs
@@ -6,6 +6,8 @@ namespace Instagram_Clone.ViewModels
     {
         public string FirstName { get; set; }
         public string LastName { get; set; }
+
+        [Unique(ErrorMessage = "Name Must Be Unique")]
         public string UserName { get; set; }
         [DataType(DataType.Password)]
         public string Password { get; set; }
---
+++ Instagram_Clone/ViewModels/UniqueAttribute.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Instagram_Clone.ViewModels
+{
+    public class UniqueAttribute : ValidationAttribute
+    {
+        protected override ValidationResult? IsValid
+            (object? value, ValidationContext validationContext)
+        {
+            
+            Context context = validationContext.GetService<Context>();
+
+            string name = value.ToString();
+            RegisterViewModel UserFromRequest = validationContext.ObjectInstance as RegisterViewModel;
+            
+            ApplicationUser UserFromDb = context.Users
+                .FirstOrDefault(user => user.UserName == name );
+            if (UserFromDb == null)
+            {
+                //value valid
+                return ValidationResult.Success;
+            }
+
+            return new ValidationResult("Name Already Found (it Must Be Unique)");
+        }
+    }
+}
