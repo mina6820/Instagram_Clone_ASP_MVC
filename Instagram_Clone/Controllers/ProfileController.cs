5f0701ac3be0d7d3885e7cd7e830379fe88d9018
Mohammed Ali <80027329@fci.s-mu.edu.eg>
2024-03-31 01:01:38 -0800

    messi edit profile

--- Instagram_Clone/Controllers/ProfileController.cs
+++ Instagram_Clone/Controllers/ProfileController.cs
@@ -1,12 +1,22 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
 
 namespace Instagram_Clone.Controllers
 {
     public class ProfileController : Controller
     {
+        private readonly Context context;
+
+        public ProfileController(Context context)
+        {
+            this.context = context;
+        }
         public IActionResult Index()
         {
-            return View();
+            string name = User.Identity.Name;
+            Claim claim = User.Claims.FirstOrDefault(c => c.Type == ClaimTypes.NameIdentifier);
+            ApplicationUser user = context.Users.FirstOrDefault(u => u.Id == claim.Value);
+            return View("Index",user);
         }
     }
 }
