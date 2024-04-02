b86fd3e3d48347692e087e92efffdf20b28a2eff
Habeba-Adnan <Habebaadnan2020@gmail.com>
2024-04-02 03:08:57 +0200

    followee popup

--- Instagram_Clone/Repositories/UserRelationship/UserRelationshipRepository.cs
+++ Instagram_Clone/Repositories/UserRelationship/UserRelationshipRepository.cs
@@ -61,6 +61,17 @@ namespace Instagram_Clone.Repositories.UserFollowRepo
             return searchedUsers;
         }
 
+        public List<ApplicationUser> searchFollowees(string Name)
+        {
+            string lowerName = Name.ToLower();
+            List<ApplicationUser> searchedUsers =
+                context.Users
+                .Include(u => u.Following)
+                .Where(u =>u.UserName.ToLower().Contains(lowerName)).ToList();
+            return searchedUsers;
+        }
+
+
         //public List<UserRelationship> searchFollowers(string name)
         //{
         //    List<UserRelationship> searchedUsers =
@@ -71,14 +82,10 @@ namespace Instagram_Clone.Repositories.UserFollowRepo
         //    return searchedUsers;
         //}
 
-        public List<ApplicationUser> searchFollowees( string Name)
-        {
-            List<ApplicationUser> users = context.Users.Where(u => u.UserName.Contains(Name)).Include(u => u.Following).ToList();
-            return users; 
-        }
 
-        
 
-        
+
+
+
     }
 }
