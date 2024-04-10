5f91b4ac234c0bde18118cfbc87c5c763143fbbe
Mohammed Ali <80027329@fci.s-mu.edu.eg>
2024-04-09 17:44:43 -0800

    end Forget Password

---
+++ Instagram_Clone/ViewModels/ForgetPasswordViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Instagram_Clone.ViewModels
+{
+    public class ForgetPasswordViewModel
+    {
+        
+        public string UserName { get; set; }
+        public string YourFavirotePerson { get; set; }
+
+        [Required(ErrorMessage = "New password is required")]
+        [DataType(DataType.Password)]
+        public string? NewPassword { get; set; }
+
+        [Required(ErrorMessage = "Confirm password is required")]
+        [DataType(DataType.Password)]
+        [Compare("NewPassword", ErrorMessage = "The new password and confirmation password do not match.")]
+        public string? ConfirmPassword { get; set; }
+    }
+}
