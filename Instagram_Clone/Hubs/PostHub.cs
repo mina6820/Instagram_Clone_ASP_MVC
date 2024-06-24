457d343cbaed1bec0111a9d4d0f8d57fced0a295
Mina Medhat <minamedhat123456@gmail.com>
2024-06-24 15:59:16 +0300

    Chat

--- Instagram_Clone/Hubs/PostHub.cs
+++ Instagram_Clone/Hubs/PostHub.cs
@@ -12,17 +12,17 @@ namespace Instagram_Clone.Hubs
         private readonly IPostRepository postRepository;
         private readonly ILikeRepository likeRepository;
 
-        public PostHub(IPostRepository postRepository , ILikeRepository likeRepository)
+        public PostHub(IPostRepository postRepository, ILikeRepository likeRepository)
         {
             this.postRepository = postRepository;
             this.likeRepository = likeRepository;
         }
 
-        public async Task AddLike(int postID , string CurrentUserId)
+        public async Task AddLike(int postID, string CurrentUserId)
         {
             Post? post = postRepository.GetPostByIDWithLikes(postID);
 
-            for(int i = 0; i < post.Likes.Count; i++)
+            for (int i = 0; i < post.Likes.Count; i++)
             {
                 if (post.Likes[i].UserId == CurrentUserId)
                     return;
@@ -38,13 +38,13 @@ namespace Instagram_Clone.Hubs
             postRepository.Update(post);
             postRepository.Save();
 
-            await Clients.All.SendAsync("ReceiveLikes", post.Id , post.Likes.Count());
+            await Clients.All.SendAsync("ReceiveLikes", post.Id, post.Likes.Count());
 
         }
 
         public async Task DeleteLike(int postID, string CurrentUserId)
         {
-            Like? like = likeRepository.GetByUserIdAndPostId(CurrentUserId , postID);
+            Like? like = likeRepository.GetByUserIdAndPostId(CurrentUserId, postID);
             like.IsDeleted = true;
             likeRepository.Update(like);
             likeRepository.Save();
@@ -56,7 +56,7 @@ namespace Instagram_Clone.Hubs
             //post.Likes.Remove(like);
             await Clients.All.SendAsync("ReceiveLikes", postID, post.Likes.Count());
         }
-        //hello 
+
 
         public async Task ShowComments(int postId)
         {
